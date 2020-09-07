d358883125985fa5099635bf219097f6e639f5a4
Андрей Ефимов <efiand@ya.ru>
2020-09-07 09:45:37 +0300

    Шаблонизация

--- src/express/index.js
+++ src/express/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const express = require(`express`);
-const {DEFAULT_PORT} = require(`../constants`);
+const path = require(`path`);
+const {DEFAULT_PORT, PUBLIC_DIR} = require(`../constants`);
 const offersRouter = require(`./routes/offers`);
 const myRouter = require(`./routes/my`);
 const indexRouter = require(`./routes/index`);
@@ -10,5 +11,16 @@ const app = express();
 app.use(`/offers`, offersRouter);
 app.use(`/my`, myRouter);
 app.use(`/`, indexRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => res.status(400).render(`400`));
+app.use((err, req, res, next) => {
+  res.status(500).render(`500`);
+  next();
+});
+
+app.set(`views`, path.resolve(__dirname, `templates/entries`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -3,32 +3,20 @@
 const {Router} = require(`express`);
 const indexRouter = new Router();
 
-const template = `
-  /
-  <br><br>
-  <a href="/offers/category/1">/offers/category/:id</a><br>
-  <a href="/offers/edit/1">/offers/edit/:id</a><br>
-  <a href="/offers/add">/offers/add</a><br>
-  <a href="/offers/1">/offers/:id</a><br>
-  <a href="/my">/my</a><br>
-  <a href="/my/comments">/my/comments</a><br>
-  <a href="/register">/register</a><br>
-  <a href="/login">/login</a><br>
-  <a href="/search">/search</a><br>
-`;
+indexRouter.get(`/login`, (req, res) => {
+  res.render(`login`);
+});
+
+indexRouter.get(`/register`, (req, res) => {
+  res.render(`sign-up`);
+});
 
 indexRouter.get(`/search`, (req, res) => {
-  res.send(`/search`);
-});
-indexRouter.get(`/login`, (req, res) => {
-  res.send(`/login`);
-});
-indexRouter.get(`/register`, (req, res) => {
-  res.send(`/register`);
+  res.render(`search-result`);
 });
 
 indexRouter.get(`/`, (req, res) => {
-  res.send(template);
+  res.render(`main`);
 });
 
 module.exports = indexRouter;
